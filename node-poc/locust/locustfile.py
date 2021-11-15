5d65e66282ac8a3ac5caf0ec7ca3877ce9b04e42
shuhanmirza <shuhan.mirza@gmail.com>
2021-11-15 09:47:58 +0600

    implement quarks locust

--- node-poc/locust/locustfile.py
+++ node-poc/locust/locustfile.py
@@ -1,15 +1,13 @@
 import random
+import time
 from datetime import datetime
 
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, tag
 
 
 class WebUser(HttpUser):
     wait_time = between(1, 5)
     host = "http://"
-
-    no_executor_api = "async/zxcvbn2"
-    customer_executor_api = "zxcvbn2"
 
     centralized_users = ['alice@org.com', 'bob@org.com', 'charlie@org.com']
     centralized_token = 'qwerty12345'
@@ -18,6 +16,14 @@ class WebUser(HttpUser):
     api_centralized_send_message = 'localhost:3000/sendMessage'
     api_centralized_read_message = 'localhost:3000/readMessage'
 
+    # -----------
+    api_quarks_send_message = 'localhost:3001/sendMessage'
+    api_quarks_read_message = 'localhost:3001/readMessage'
+
+    quarks_users = ['alice@org1.com']
+    quarks_channels = ['channel-123']
+
+    @tag('centralized')
     @task
     def centralized_send_message(self):
         user_email = random.choice(self.centralized_users)
@@ -36,6 +42,7 @@ class WebUser(HttpUser):
 
         self.client.post(url=self.api_centralized_send_message, json=json_body, headers=headers)
 
+    @tag('centralized')
     @task
     def centralized_read_message(self):
         user_email = random.choice(self.centralized_users)
@@ -52,5 +59,37 @@ class WebUser(HttpUser):
 
         self.client.post(url=self.api_centralized_read_message, json=json_body, headers=headers)
 
+    @tag('quarks')
+    @task
+    def quarks_send_message(self):
+        user_email = random.choice(self.quarks_users)
+        user_channel = random.choice(self.quarks_channels)
+        user_text = self.generate_text(username=user_email)
+
+        json_body = {
+            "user": user_email,
+            "text": user_text,
+            "channel": user_channel
+        }
+
+        self.client.post(url=self.api_quarks_send_message, json=json_body)
+
+    @tag('quarks')
+    @task
+    def quarks_read_message(self):
+        user_email = random.choice(self.quarks_users)
+        user_channel = random.choice(self.quarks_channels)
+
+        json_body = {
+            "user": user_email,
+            "channel": user_channel,
+            "from_timestamp": self.get_timestamp()
+        }
+
+        self.client.get(url=self.api_quarks_read_message, json=json_body)
+
     def generate_text(self, username):
         return 'I am {}. current time is {}'.format(username, datetime.now())
+
+    def get_timestamp(self):
+        return f'{(time.time() - 600.00) * 1000000000:.0f}'
